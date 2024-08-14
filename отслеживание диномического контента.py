e663702fb15851303a37c80609bbb73e2122e5bf
SerzhSeryozhka <JK3103KS@ya.ru>
2024-08-14 14:20:13 +0300

    НАчало в автотестинге. И изменение настроек автосохранения кода.
    Signed-off-by: SerzhSeryozhka <JK3103KS@ya.ru>

--- отслеживание диномического контента.py
+++ отслеживание диномического контента.py
@@ -12,15 +12,14 @@ driver.get("https://erikdark.github.io/QA_autotest_16/")
 
 # Ждем, пока цена Лады станет 550 долларов
 wait = WebDriverWait(driver, 10)
-price_element = wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, "#car-card"), "550"))
+price_element = wait.until(EC.text_to_be_present_in_element((By.ID, 'price1'), "550"))
 
 # Нажимаем на кнопку
-button = driver.find_element(By.CSS_SELECTOR, "button")
-button.click()
+button = driver.find_element(By.ID, "buyButton1").click()
 
 # Проверяем текст в блоке
-result_text = driver.find_element(By.CSS_SELECTOR, "#result").text
-assert result_text == "Wonderful!"
+result_text = driver.find_element(By.CSS_SELECTOR, "#message1").text
+assert result_text == "Вы купили автомобиль!"
 
 # Ждем 5 секунд, затем закрываем браузер
 time.sleep(5)
--- переход на новую вкладку с помощью селениума.py
+++ переход на новую вкладку с помощью селениума.py
@@ -7,19 +7,27 @@ try:
     driver = webdriver.Chrome()
 
     driver.get('https://erikdark.github.io/QA_autotests_13/')
+    time.sleep(5)
     print('Стр загрузилась')
     button = driver.find_element(By.ID, 'openNewPageBtn').click()
+    time.sleep(5)
     print('Кнпк1 нажата')
-    new_tab = driver.window_handles[1]
+    new_tab = driver.window_handles[5]
+    time.sleep(5)
     print('Найдено новое окно')
     driver.switch_to.window(new_tab)
+    time.sleep(5)
     print('Перейдено в новое окно')
     button1 = driver.find_element(By. ID, 'displayTextBtn').click()
+    time.sleep(5)
     print('Кнп2 нажата')
     disp_txt = driver.find_element(By.ID,'displayText').text
+    time.sleep(5)
     print('Текст получен')
     assert disp_txt == 'Я СДЕЛАЛ',f'Текст не совпадает: ожидалось Я СДЕЛАЛ, но было {disp_txt}'
+    time.sleep(5)
     print('Текст == ',disp_txt)
+    time.sleep(5)
 finally:
     time.sleep(3)
     driver.quit()
